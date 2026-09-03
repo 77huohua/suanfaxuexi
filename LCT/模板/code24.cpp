88141746e491c36358bb985daa094366aecf2758
77huohua <2741686364@qq.com>
2026-09-03 14:52:52 +0800

    26.9.3.1

---
+++ LCT/模板/code24.cpp
@@ -0,0 +1,249 @@
+// 初始时有n个互不连通的点，然后有m条操作，操作类型如下
+// 操作 A x y : 点x和点y之间连一条边，保证之前x和y是不连通的
+// 操作 Q x   : 打印点x所在连通区的重心编号
+// 操作 Xor   : 打印当前所有连通区，重心编号的异或和
+// 本题的一个连通区就是一棵树，重心是到其他点的距离总和最小的点
+// 为了保证重心唯一，本题规定，如果有多个重心，选择编号最小的点作为重心
+// 1 <= n <= 10^5
+// 1 <= m <= 2 * 10^5
+// 测试链接 : https://www.luogu.com.cn/problem/P4299
+
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<vector>
+#include<climits>
+#include<cmath>
+#include<queue>
+#include<cstring>
+#include<bit>
+using namespace std;
+using ll = long long;
+
+const int MAXN = 100010;
+int n, m;
+
+int fa[MAXN];
+int ls[MAXN];
+int rs[MAXN];
+bool rev[MAXN];
+int sta[MAXN];
+
+int father[MAXN];
+int vir[MAXN];
+int sum[MAXN];
+
+int xorsum;
+
+int find(int x) {
+	if (x != father[x]) {
+		father[x] = find(father[x]);
+	}
+	return father[x];
+}
+
+void up(int x) {
+	sum[x] = sum[ls[x]] + sum[rs[x]] + vir[x] + 1;
+}
+
+bool isroot(int x) {
+	return ls[fa[x]] != x && rs[fa[x]] != x;
+}
+
+int lr(int x) {
+	return ls[fa[x]] == x ? 0 : 1;
+}
+
+void reverse(int x) {
+	if (x != 0) {
+		swap(ls[x], rs[x]);
+		rev[x] = !rev[x];
+	}
+}
+
+void down(int x) {
+	if (rev[x]) {
+		reverse(ls[x]);
+		reverse(rs[x]);
+		rev[x] = false;
+	}
+}
+
+void rotate(int x) {
+	int f = fa[x], g = fa[f];
+	if (lr(x) == 0) {
+		ls[f] = rs[x];
+		if (ls[f] != 0) {
+			fa[ls[f]] = f;
+		}
+		rs[x] = f;
+	}
+	else
+	{
+		rs[f] = ls[x];
+		if (rs[f] != 0) {
+			fa[rs[f]] = f;
+		}
+		ls[x] = f;
+	}
+	if (!isroot(f)) {
+		if (lr(f) == 0) {
+			ls[g] = x;
+		}
+		else
+		{
+			rs[g] = x;
+		}
+	}
+	fa[f] = x;
+	fa[x] = g;
+	up(f);
+	up(x);
+}
+
+void splay(int x) {
+	int siz = 0;
+	sta[++siz] = x;
+	for (int y = x;!isroot(y);y = fa[y]) {
+		sta[++siz] = fa[y];
+	}
+	while (siz!=0)
+	{
+		down(sta[siz--]);
+	}
+	while (!isroot(x))
+	{
+		int f = fa[x];
+		if (!isroot(f)) {
+			if (lr(x) == lr(f)) {
+				rotate(f);
+			}
+			else
+			{
+				rotate(x);
+			}
+		}
+		rotate(x);
+	}
+}
+
+void access(int x) {
+	for (int y = 0;x != 0;y = x, x = fa[x]) {
+		splay(x);
+		vir[x] += sum[rs[x]];
+		vir[x] -= sum[y];
+		rs[x] = y;
+		up(x);
+	}
+}
+
+void makeroot(int x) {
+	access(x);
+	splay(x);
+	reverse(x);
+}
+
+int findroot(int x) {
+	access(x);
+	splay(x);
+	down(x);
+	while (ls[x]!=0)
+	{
+		x = ls[x];
+		down(x);
+	}
+	splay(x);
+	return x;
+}
+
+void split(int x, int y) {
+	makeroot(x);
+	access(y);
+	splay(y);
+}
+
+void link(int x, int y) {
+	makeroot(x);
+	if (findroot(y) != x) {
+		access(y);
+		splay(y);
+		fa[x] = y;
+		vir[y] += sum[x];
+		up(y);
+	}
+}
+
+/*void cut(int x, int y) {
+	makeroot(x);
+	if (findroot(y) == x && fa[y] == x && ls[y] == 0 && rs[x] == y) {
+		fa[y] = rs[x] = 0;
+		up(x);
+	}
+}
+*/
+
+int newCenter(int a, int b) {
+	split(a, b);
+	int half = sum[b] >> 1;
+	int lpass = 0;
+	int rpass = 0;
+	int ans = n + 1;
+	int cur = b;
+	while (cur!=0)
+	{
+		down(cur);
+		int lsiz = sum[ls[cur]] + lpass;
+		int rsiz = sum[rs[cur]] + rpass;
+		if (lsiz <= half && rsiz <= half) {
+			ans = min(ans, cur);
+		}
+		if (lsiz < rsiz) {
+			lpass += sum[ls[cur]] + vir[cur] + 1;
+			cur = rs[cur];
+		}
+		else
+		{
+			rpass += sum[rs[cur]] + vir[cur] + 1;
+			cur = ls[cur];
+		}
+	}
+	splay(ans);
+	return ans;
+}
+
+void road(int x, int y) {
+	int a = find(x);
+	int b = find(y);
+	link(x, y);
+	int cur = newCenter(a, b);
+	father[cur] = father[a] = father[b] = cur;
+	xorsum ^= a ^ b ^ cur;
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1;i <= n;i++) {
+		sum[i] = 1;
+		father[i] = i;
+		xorsum ^= i;
+	}
+	string op;
+	int x, y;
+	for (int i = 1;i <= m;i++) {
+		cin >> op;
+		if (op == "A") {
+			cin >> x >> y;
+			road(x, y);
+		}
+		else if (op == "Q") {
+			cin >> x;
+			cout << find(x) << endl;
+		}
+		else
+		{
+			cout << xorsum << endl;
+		}
+	}
+	return 0;
+}
