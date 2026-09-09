b5bf61de3bfdae330178d9ec1668386cd5467b8f
77huohua <2741686364@qq.com>
2026-09-09 18:16:06 +0800

    26.9.9.19

---
+++ 刷题/luogu/P5704.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<vector>
+#include<climits>
+#include<cmath>
+#include<queue>
+#include<cstring>
+#include<bit>
+#include<memory>
+#include<bitset>
+using namespace std;
+
+char c;
+
+int main() {
+	cin >> c;
+	cout << (char)(c + 'A' - 'a') << endl;
+}
