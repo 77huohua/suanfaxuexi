5f4068a46c9db18fa7de6c0fcbd66042ba8b7ecf
77huohua <2741686364@qq.com>
2026-09-09 18:13:48 +0800

    26.9.9.17

---
+++ 刷题/luogu/B2005.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<vector>
+#include<climits>
+#include<cmath>
+#include<queue>
+#include<cstring>
+#include<bit>
+#include<memory>
+#include<bitset>
+using namespace std;
+
+char a;
+
+int main() {
+	cin >> a;
+	cout << "  " << a << endl;
+	cout << ' ' << a << a << a << endl;
+	cout << a << a << a << a << a << endl;
+}
