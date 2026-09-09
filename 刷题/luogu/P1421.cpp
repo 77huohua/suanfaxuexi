d8a3c2a106d60aaf0c55493e9322befa6b7028ed
77huohua <2741686364@qq.com>
2026-09-09 18:20:32 +0800

    26.9.9.25

---
+++ 刷题/luogu/P1421.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<vector>
+#include<climits>
+#include<cmath>
+#include<queue>
+#include<cstring>
+#include<bit>
+#include<memory>
+#include<bitset>
+#include<string>
+#include<format>
+using namespace std;
+
+int a, b;
+
+int main() {
+	cin >> a >> b;
+	cout << (a * 10 + b) / 19 << endl;
+}
