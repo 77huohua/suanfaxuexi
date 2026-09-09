bf46aa3c4a228ca686e54ea29028cc6eabcf27d1
77huohua <2741686364@qq.com>
2026-09-09 18:17:34 +0800

    26.9.9.21

---
+++ 刷题/luogu/P5706.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<vector>
+#include<climits>
+#include<cmath>
+#include<queue>
+#include<cstring>
+#include<bit>
+#include<memory>
+#include<bitset>
+#include<string>
+using namespace std;
+
+double t;
+int n;
+
+int main() {
+	cin >> t >> n;
+	printf("%.3f\n", t / n);
+	cout << n * 2;
+}
