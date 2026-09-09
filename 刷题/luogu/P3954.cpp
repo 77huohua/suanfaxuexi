2ed64025c2e2e6b32c6d0c0db1d9ce7809c2dd9e
77huohua <2741686364@qq.com>
2026-09-09 18:21:02 +0800

    26.9.9.26

---
+++ 刷题/luogu/P3954.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<vector>
+#include<climits>
+#include<cmath>
+#include<queue>
+#include<cstring>
+#include<bit>
+#include<memory>
+#include<bitset>
+#include<string>
+#include<format>
+using namespace std;
+
+int A, B, C;
+
+int main() {
+	cin >> A >> B >> C;
+	cout << A * 0.2 + B * 0.3 + C * 0.5 << endl;
+}
