023930addf706234af152396ed57c1ca17a0754a
77huohua <2741686364@qq.com>
2026-09-15 11:59:05 +0800

    26.9.15.1

---
+++ K-D树/code03.cpp
@@ -0,0 +1,118 @@
+// 一共n个点，每个点有坐标(x, y)，还有点权v
+// 一共m条查询，格式 a b c，含义如下
+// 满足 a * x + b * y < c 的所有点，打印点权累加和
+// 1 <= n、m <= 5 * 10^4
+// -10^9 <= a、b、x、y <= +10^9
+// 测试链接 : https://www.luogu.com.cn/problem/P4475
+
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<vector>
+#include<climits>
+#include<cmath>
+#include<queue>
+#include<cstring>
+#include<bit>
+#include<memory>
+#include<bitset>
+#include<string>
+#include<format>
+using namespace std;
+using ll = long long;
+
+const int MAXN = 50010;
+int n,m;
+ll x[MAXN];
+ll y[MAXN];
+ll v[MAXN];
+int arr[MAXN];
+
+int root;
+int ls[MAXN];
+int rs[MAXN];
+
+ll xmin[MAXN];
+ll xmax[MAXN];
+ll ymin[MAXN];
+ll ymax[MAXN];
+ll sum[MAXN];
+
+int compareNode(int i, int j, int dimension) {
+	ll v1 = dimension == 0 ? x[i] : y[i];
+	ll v2 = dimension == 0 ? x[j] : y[j];
+	return v1 == v2 ? 0 : v1 < v2 ? -1 : 1;
+}
+
+struct Cmp
+{
+	int dimension;
+	bool operator()(int a, int b) const {
+		return compareNode(a, b, dimension) < 0;
+	}
+};
+
+void maintain(int i) {
+	sum[i] = v[i] + sum[ls[i]] + sum[rs[i]];
+	xmin[i] = min({ x[i],xmin[ls[i]],xmin[rs[i]] });
+	xmax[i] = max({ x[i],xmax[ls[i]],xmax[rs[i]] });
+	ymin[i] = min({ y[i],ymin[ls[i]],ymin[rs[i]] });
+	ymax[i] = max({ y[i],ymax[ls[i]],ymax[rs[i]] });
+}
+
+int build(int l, int r, int dimension) {
+	if (l > r) {
+		return 0;
+	}
+	int mid = (l + r) >> 1;
+	nth_element(arr + l, arr + mid, arr + r + 1, Cmp{ dimension });
+	int rt = arr[mid];
+	ls[rt] = build(l, mid - 1, dimension ^ 1);
+	rs[rt] = build(mid + 1, r, dimension ^ 1);
+	maintain(rt);
+	return rt;
+}
+
+ll query(ll a, ll b, ll c, int i) {
+	if (i == 0) {
+		return 0;
+	}
+	ll ax1 = xmin[i] * a;
+	ll ax2 = xmax[i] * a;
+	ll by1 = ymin[i] * b;
+	ll by2 = ymax[i] * b;
+	ll minv = min(ax1, ax2) + min(by1, by2);
+	ll maxv = max(ax1, ax2) + max(by1, by2);
+	if (minv >= c) {
+		return 0;
+	}
+	if (maxv < c) {
+		return sum[i];
+	}
+	ll ans = 0;
+	if (a * x[i] + b * y[i] < c) {
+		ans += v[i];
+	}
+	ans += query(a, b, c, ls[i]);
+	ans += query(a, b, c, rs[i]);
+	return ans;
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1;i <= n;i++) {
+		cin >> x[i] >> y[i] >> v[i];
+		arr[i] = i;
+	}
+	xmin[0] = ymin[0] = LLONG_MAX;
+	xmax[0] = ymax[0] = LLONG_MIN;
+	root = build(1, n, 0);
+	ll a, b, c;
+	for (int i = 1;i <= m;i++) {
+		cin >> a >> b >> c;
+		cout << query(a, b, c, root) << endl;
+	}
+	return 0;
+}
+
